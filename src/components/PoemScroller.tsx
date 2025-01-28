0bda72c04a69c002d193a0cea4671db7ae0864fe
palmdrop <anton@exlex.se>
2025-01-28 10:32:18 +0100

    feat: add scrolling to poem scroller, fix alignment, etc

--- src/components/PoemScroller.tsx
+++ src/components/PoemScroller.tsx
@@ -1,8 +1,18 @@
-import { For, Show } from 'solid-js';
+import { createEffect, For, Show } from 'solid-js';
 import LineRenderer from './LineRenderer';
 import { IndexedLines } from '~/app';
 
 const PoemScroller = (props: { index: number; lines: IndexedLines }) => {
+  const lines = new Map<number, HTMLParagraphElement>();
+
+  createEffect(() => {
+    const line = lines.get(props.index);
+    if (!line) return;
+
+    // TODO: only scroll IF its sufficiently close to the edge... and add some offset!
+    line.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  }, [props.index]);
+
   return (
     <article class="poem-scroller">
       <For each={props.lines}>
@@ -11,6 +21,10 @@ const PoemScroller = (props: { index: number; lines: IndexedLines }) => {
             classList={{
               line: true,
               active: indexedLine?.[0] === props.index
+            }}
+            ref={element => {
+              if (!element || !indexedLine) return;
+              lines.set(indexedLine?.[0], element);
             }}
           >
             <Show when={!!indexedLine} fallback={<br />}>
