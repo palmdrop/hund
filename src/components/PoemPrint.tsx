829e2afc51b55ffe7f489b9bc65677ff35900525
palmdrop <anton@exlex.se>
2025-02-05 22:33:06 +0100

    feat: print tests and styling changes

--- src/components/PoemPrint.tsx
+++ src/components/PoemPrint.tsx
@@ -3,6 +3,8 @@ import PoemRenderer from './PoemRenderer';
 
 import '../styles/print.css';
 
+// import image from '../images/strand1.jpg';
+
 const src =
   'https://images.unsplash.com/photo-1520454974749-611b7248ffdb?q=80&w=3648&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
@@ -12,6 +14,7 @@ const PoemPrint = (props: { lines: Lines }) => {
       <div class="print-image">
         <img src={src} />
       </div>
+      <div class="small-sphere"></div>
       <div class="print-poem">
         <PoemRenderer lines={props.lines} />
       </div>
